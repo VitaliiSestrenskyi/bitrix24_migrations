fbd769c1775f37535043a0c5747f1d4047ee1f64
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2018-09-17 10:29:39 +0300

    Update addColumnToTable.php

--- addColumnToTable.php
+++ addColumnToTable.php
@@ -1,18 +1,17 @@
 <?php
-use Bitrix\Main\Application;
 
 class addColumnToTable extends AbstractMigration
 {
     public function up()
     {
-         $connection = Application::getInstance()->getConnection();
+         $connection = \Bitrix\Main\Application::getInstance()->getConnection();
 
         $sql = "ALTER TABLE `crm_table_name` ADD `NEW_COLUMN_NAME` varchar(255) AFTER `DISCOUNT`;";
         $connection->queryExecute($sql);
     }
     public function down()
     {
-        $connection = Application::getInstance()->getConnection();
+        $connection =  \Bitrix\Main\Application::getInstance()->getConnection();
 
         $sql = "ALTER TABLE `crm_table_name` DROP `NEW_COLUMN_NAME`;";
         $connection->queryExecute($sql);
