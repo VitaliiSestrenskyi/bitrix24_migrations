2b30d2fb081e1eabd3e0059fd8b5b1d2313f6acc
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2019-03-26 11:01:14 +0200

    installUninstall.php
    installUninstall.php

---
+++ installUninstall.php
@@ -0,0 +1,34 @@
+<?php
+class Module extends AbstractMigration
+{
+    public function up()
+    {
+        \Bitrix\Main\Loader::includeModule('main');
+        $id = "test.test";
+        $Module = \CModule::CreateModuleObject($id);
+        if($Module)
+        {
+            if(\COption::GetOptionString("main", "event_log_marketplace", "Y") === "Y")
+            {
+                \CEventLog::Log("INFO", "MP_MODULE_INSTALLED", "main", $id);
+            }
+            $Module->DoInstall();
+        }
+    }
+    
+    public function down()
+    {
+        \Bitrix\Main\Loader::includeModule('main');
+        $id = "test.test"
+        $Module = \CModule::CreateModuleObject($id);
+        if($Module)
+        {
+            if(\COption::GetOptionString("main", "event_log_marketplace", "Y") === "Y")
+            {
+                \CEventLog::Log("INFO", "MP_MODULE_UNINSTALLED", "main", $id);
+            }
+            $Module->DoUninstall();
+        }
+    }
+    
+}
